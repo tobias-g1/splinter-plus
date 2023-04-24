fd62405b003f3d6a66c9fdef90334feb2b70816c
tobias-g1 <tobias.gooney@icloud.com>
2023-04-24 20:40:29 +0100

    Add the ability to auto-claim SPS rewards every 1 hour (#1)
    * Update .gitignore
    
    * Update messages.json
    
    * Update index.ts
    
    * Update manifest.json
    
    * Fix settings
    
    * Split code
    
    * Adjust every 60 minutes
    
    * Delete settings.ts
    
    * Delete util.ts
    
    * Update manifest.json
    
    * Update messages.json
    
    * Update plugin.ts

---
+++ src/background/claim.ts
@@ -0,0 +1,32 @@
+import { sendCustomJSONRequest } from "@background/keychain";
+
+export const checkAutoClaimSetting = async () => {
+  const data = await chrome.storage.local.get('plugindata');
+  const users = data?.plugindata?.userSettings || {};
+  for (const user in users) {
+    const autoClaimSettingEnabled = users[user].autoClaimSetting;
+    if (autoClaimSettingEnabled) {
+      console.log(`Auto claim setting is enabled for ${user}`);
+      try {
+        await createClaim(user);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  }
+};
+
+export const createClaim = async (username: string): Promise<any> => {
+
+  console.log(`Creating claim for user ${username}`);
+  const json: string = JSON.stringify({
+    token: 'SPS',
+    qty: 0,
+    app: 'splinter-plus',
+    n: '19nqfUoKHV'
+  })
+
+  const claim = await sendCustomJSONRequest('sm_stake_tokens', json, username);
+  console.log(`Claim created for user ${username}:`, claim);
+  return claim;
+};
---
+++ src/background/keychain.ts
@@ -0,0 +1,29 @@
+import {
+  KeychainKeyTypes,
+  KeychainRequest,
+  KeychainRequestTypes
+} from 'src/interfaces/keychain.interface';
+
+const KEYCHAIN_EXTENSION_ID = 'fbjjilfdeadgdphnoikkhjhpnndlkgfn';
+
+const generateSafeRandomNumber = (): number => {
+  return Math.floor(Math.random() * 1000000);
+};
+
+export const sendCustomJSONRequest = async (
+  id: string, json: string, username: string
+): Promise<any> => {
+  return new Promise((resolve, reject) => {
+        const request = {
+          type: KeychainRequestTypes.custom,
+          id,
+          method: KeychainKeyTypes.posting,
+          json,
+          username,
+          request_id: generateSafeRandomNumber()
+      } as KeychainRequest;
+    chrome.runtime.sendMessage(KEYCHAIN_EXTENSION_ID, request, (response) => {
+      resolve(response);
+    });
+  });
+};
