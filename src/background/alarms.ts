c1f4c91b0503d9c00b03c265bea93692edc22af2
tobias-g1 <tobias.gooney@icloud.com>
2023-04-27 20:33:08 +0100

    Update how we refresh prices

--- src/background/alarms.ts
+++ src/background/alarms.ts
@@ -1,9 +1,7 @@
 
-import { fetchPricesAndUpdateStorage } from 'src/common/prices';
 import { fetchSettingsAndUpdateStorage } from 'src/common/settings';
 import { checkAutoClaimSetting } from './claim';
 
-const PRICES_REFRESH_INTERVAL_MINUTES = Number(process.env.PRICES_REFRESH_INTERVAL_MINUTES) || 1;
 const SETTINGS_REFRESH_INTERVAL_MINUTES = Number(process.env.SETTINGS_REFRESH_INTERVAL_MINUTES) || 240;
 const CHECK_AUTO_CLAIM_SETTING_INTERVAL_MINUTES = Number(process.env.CHECK_AUTO_CLAIM_SETTING_INTERVAL_MINUTES) || 60;
 
@@ -12,9 +10,6 @@ const CHECK_SETTINGS_ALARM = 'checkSettings';
 const CHECK_AUTO_CLAIM_SETTING_ALARM = 'checkAutoClaimSetting';
 
 export const createAlarms = () => {
-  chrome.alarms.create(CHECK_PRICES_ALARM, {
-    periodInMinutes: PRICES_REFRESH_INTERVAL_MINUTES,
-  });
   chrome.alarms.create(CHECK_SETTINGS_ALARM, {
     periodInMinutes: SETTINGS_REFRESH_INTERVAL_MINUTES,
   });
@@ -24,9 +19,7 @@ export const createAlarms = () => {
 };
 
 export const handleAlarm = async (alarm: chrome.alarms.Alarm) => {
-  if (alarm.name === CHECK_PRICES_ALARM) {
-    await fetchPricesAndUpdateStorage();
-  } else if (alarm.name === CHECK_SETTINGS_ALARM) {
+  if (alarm.name === CHECK_SETTINGS_ALARM) {
     await fetchSettingsAndUpdateStorage();
   } else if (alarm.name === CHECK_AUTO_CLAIM_SETTING_ALARM) {
     await checkAutoClaimSetting();
--- src/common/prices.ts
+++ src/common/prices.ts
@@ -1,29 +1,61 @@
 import { Prices } from "src/interfaces/prices.interface";
 
+const TIME_INTERVAL_IN_MINUTES = Number(process.env.TIME_INTERVAL_IN_MINUTES) || 1;
+const BASE_URL = process.env.BASE_URL || "https://prices.splinterlands.com";
+
 export const fetchPrices = async (): Promise<Prices> => {
-    const response = await fetch('https://prices.splinterlands.com/prices');
-    const prices: Prices = await response.json();
-    return prices;
+    const lastRefresh = await getLastPriceRefresh();
+    const currentTime = new Date().getTime();
+    if (!lastRefresh || currentTime - lastRefresh > TIME_INTERVAL_IN_MINUTES * 60 * 1000) {
+        const response = await fetch(`${BASE_URL}/prices`);
+        const prices: Prices = await response.json();
+        await updatePrices(prices);
+        return prices;
+    } else {
+        const cachedPrices = await getCachedPrices();
+        return cachedPrices || fetchPrices();
+    }
 };
 
-export const fetchPricesAndUpdateStorage = async () => {
-    const prices: Prices = await fetchPrices();
-    chrome.storage.local.set({ prices }, () => {
+const getLastPriceRefresh = (): Promise<number | undefined> => {
+    return new Promise<number | undefined>((resolve) => {
+        chrome.storage.local.get('lastPriceRefresh', (data) => {
+            const lastRefresh: number | undefined = data.lastPriceRefresh;
+            resolve(lastRefresh);
+        });
+    });
+};
+
+const updatePrices = async (prices: Prices) => {
+    chrome.storage.local.set({ prices, lastPriceRefresh: new Date().getTime() }, () => {
         if (chrome.runtime.lastError) {
             console.error('Error saving prices data:', chrome.runtime.lastError);
         }
     });
 };
 
-export const getPricesFromLocalStorage = async (): Promise<Prices | undefined> => {
+const getCachedPrices = (): Promise<Prices | undefined> => {
     return new Promise<Prices | undefined>((resolve) => {
-        chrome.storage.local.get('prices', async (data) => {
-            let prices: Prices | undefined = data.prices;
-            if (!prices) {
-                prices = await fetchPrices();
-                fetchPricesAndUpdateStorage();
-            }
+        chrome.storage.local.get('prices', (data) => {
+            const prices: Prices | undefined = data.prices;
             resolve(prices);
         });
     });
+};
+
+export const getPricesFromLocalStorage = async (): Promise<Prices | undefined> => {
+    const lastRefresh = await getLastPriceRefresh();
+    const currentTime = new Date().getTime();
+    if (!lastRefresh || currentTime - lastRefresh > TIME_INTERVAL_IN_MINUTES * 60 * 1000) {
+        const prices = await fetchPrices();
+        return prices;
+    } else {
+        const cachedPrices = await getCachedPrices();
+        if (!cachedPrices) {
+            const prices = await fetchPrices();
+            return prices;
+        } else {
+            return cachedPrices;
+        }
+    }
 };
