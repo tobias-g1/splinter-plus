b52ddb837f52a11dbe5aab34d0f481960cbc516d
tobias-g1 <tobias.gooney@icloud.com>
2023-07-06 10:36:50 +0100

    Working communication between background and combine

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,13 +1,46 @@
 import { PluginMessage } from 'hive-keychain-commons/lib/plugins';
 import { handleKeyChainResponse } from 'src/common/keychain-response';
+import { verifySuccessfulPurchases } from 'src/common/splinterlands';
 import { createAlarms, handleAlarm } from './alarms';
 import { sendPluginData } from './plugin';
 
-const externalMessageHandler = async (
-  message: any,
-  sender: chrome.runtime.MessageSender,
-  sendResp: (response?: any) => void,
-) => {
+let contentScriptPort: chrome.runtime.Port | null = null;
+let contentScriptReady = false;
+
+chrome.runtime.onConnect.addListener((port) => {
+  if (port.name === "content-script") {
+    contentScriptPort = port;
+
+    port.onMessage.addListener((message) => {
+      console.log("Message from content script:", message);
+      if (message.command === 'activeTab') {
+        const activeTabId = message.tabId;
+        console.log(`Tab ID received and stored: ${activeTabId}`);
+        port.postMessage({ status: 'Tab ID received' });
+      } else if (message.command === 'contentReady') {
+        contentScriptReady = true;
+        console.log('Content script is ready.');
+      }
+      // Handle other messages...
+    });
+
+    // Notify the content script that the background script is ready
+    port.postMessage({ command: 'backgroundReady' });
+    console.log('Sent backgroundReady message to content script');
+  }
+});
+
+// Function to send messages to the content script
+export const sendMessageToContentScript = (command: any) => {
+  if (contentScriptPort && contentScriptReady) {
+    contentScriptPort.postMessage(command);
+    console.log('Message sent to content script:', command);
+  } else {
+    console.error("No connection to content script or content script is not ready.");
+  }
+};
+
+const externalMessageHandler = async (message: any, sender: any, sendResp: any) => {
   if (message.command === PluginMessage.IS_INSTALLED) {
     sendResp(PluginMessage.ACK_PLUGIN_INSTALL);
   } else if (message.command === PluginMessage.GET_PLUGIN_INFO) {
@@ -18,10 +51,8 @@ const externalMessageHandler = async (
     chrome.storage.local.set({ plugindata: message.value }, () => {
       if (chrome.runtime.lastError) {
         console.error('Error saving data:', chrome.runtime.lastError);
-        sendResp(PluginMessage.ACK_PLUGIN_DATA_SAVED);
-      } else {
-        sendResp(PluginMessage.ACK_PLUGIN_DATA_SAVED);
       }
+      sendResp(PluginMessage.ACK_PLUGIN_DATA_SAVED);
     });
   } else {
     console.log(message);
@@ -37,9 +68,7 @@ chrome.alarms.onAlarm.addListener(async (alarm) => {
   handleAlarm(alarm);
 });
 
-export const validate = async (user: string, trxId: string): Promise<any> => {
-  // Communicate back to content script
-}
 
+verifySuccessfulPurchases('cdba4d044be139bfb6a67658bd6c287d6ab4c20c')
 
-console.log('Background Script Loaded')
+console.log('Background Script Loaded');
--- src/content-scripts/combine/index.ts
+++ src/content-scripts/combine/index.ts
@@ -1,20 +1,50 @@
-import { addConversionButton } from "src/content-scripts/combine/add-button";
+import { addCombineButton } from "src/content-scripts/combine/add-button";
+import { showLoadingIndicator } from "src/content-scripts/combine/combine-modal";
 import './combine.scss';
 
+let backgroundScriptPort: chrome.runtime.Port;
+
+const connectToBackgroundScript = () => {
+  backgroundScriptPort = chrome.runtime.connect({ name: 'content-script' });
+
+  backgroundScriptPort.onMessage.addListener((message) => {
+    console.log("Message from background script:", message);
+    if (message.command === 'backgroundReady') {
+      console.log('Background script is ready.');
+      // Notify the background script that the content script is ready
+      backgroundScriptPort.postMessage({ command: 'contentReady' });
+      console.log('Sent contentReady message to background script');
+    } else if (message.command === 'showLoadingIndicator') {
+      showLoadingIndicator()
+    }
+    // Handle other messages...
+  });
+
+  // Notify the background script that the content script is ready
+  backgroundScriptPort.postMessage({ command: 'backgroundReady' });
+  console.log('Sent backgroundReady message to background script');
+};
+
 /**
- * Checks for the existence of the conversion button and adds it if it doesn't already exist.
+ * Checks for the existence of the conversion button in the specified classes and adds it if it doesn't already exist.
  */
 const checkButtonsExist = () => {
-  const buttonsDivs = document.querySelectorAll('.buttons');
+  const buttonsDivs = document.querySelectorAll('.buttons, .c-PJLV-ifKYhuQ-css > .c-PJLV-ihmcGFm-css');
   if (buttonsDivs && buttonsDivs.length) {
-    addConversionButton();
+    addCombineButton();
+    console.log('Conversion button added.');
   } else {
     return;
   }
 };
 
+
+// Connect to the background script
+connectToBackgroundScript();
+
 // Call the checkButtonsExist function on initial load
 checkButtonsExist();
+console.log('Content script loaded successfully.');
 
 // Watch for changes to the DOM and check for the existence of the conversion button on each change
 const observer = new MutationObserver(() => {
@@ -25,5 +55,3 @@ observer.observe(document.body, {
   childList: true,
   subtree: true
 });
-
-console.log('Content script loaded successfully.');
