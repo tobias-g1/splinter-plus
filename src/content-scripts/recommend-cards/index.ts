bef020e02d3696c3f91e51404500941bfc3d1ceb
tobias-g1 <tobias.gooney@icloud.com>
2023-07-13 13:44:39 +0100

    Get format

--- src/content-scripts/recommend-cards/index.ts
+++ src/content-scripts/recommend-cards/index.ts
@@ -4,16 +4,19 @@ import './panel.scss';
 
 const battleHistoryUrl = 'https://splinterlands.com/?p=battle_history';
 let inProgress = false;
+let format: string = '';
 
 // Check if the current page is the battle history page
 if (window.location.href === battleHistoryUrl) {
 
-    const formatElement: HTMLElement | null = document.querySelector('.bh-selectable-obj a.selected');
-    console.log(formatElement)
-    let format = '';
+    function extractFormat(): string {
+        const formatElement = document.querySelector('.bh-selectable-obj a.selected') as HTMLElement | null;
 
-    if (formatElement) {
-        format = formatElement.innerText.toLowerCase();
+        if (formatElement) {
+            format = formatElement.innerText.trim().toLowerCase();
+        }
+
+        return format;
     }
 
     // Function to check for the existence of the history-header div and add the panel if it doesn't already exist
@@ -27,6 +30,8 @@ if (window.location.href === battleHistoryUrl) {
             // Disconnect the observer
             observer.disconnect();
 
+            format = extractFormat(); // Extract format again before building panel
+
             await buildAndInsertPanel(format);
             observer.observe(document.body, {
                 childList: true,
@@ -36,7 +41,6 @@ if (window.location.href === battleHistoryUrl) {
 
         inProgress = false;
     };
-
 
     // Call the checkPanelExists function on initial load
     checkPanelExists();
