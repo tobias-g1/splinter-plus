4e3ffd238bcb2be9498511c2b1691914dd44f040
tobias-g1 <tobias.gooney@icloud.com>
2023-07-14 13:06:31 +0100

    Revert "Minor ajdustments to deck layout"
    This reverts commit 3e1f6f2db81026998e7097ec079ac633859fa7ef.

--- src/content-scripts/recommend-deck/index.ts
+++ src/content-scripts/recommend-deck/index.ts
@@ -1,7 +1,12 @@
+import { getDecks } from 'src/common/splinter-plus';
+import { getCardDetails, getCollection } from 'src/common/splinterlands';
+import { getUsernameFromLocalStorage } from 'src/common/user';
+import { DeckResponse } from 'src/interfaces/spinter-plus.interface';
+import { Card, CardDetail, CardDetailOwnership, Collection } from 'src/interfaces/splinterlands.interface';
 import '../../styles/common.scss';
 import '../../styles/deck.scss';
 import '../../styles/modal.scss';
-import { createHeader } from '../common/common';
+import { createCardItem, createHeader } from '../common/common';
 
 const battleHistoryUrl = 'https://splinterlands.com/?p=create_team2';
 let format: string = 'wild';
@@ -17,7 +22,7 @@ if (window.location.href === battleHistoryUrl) {
     // Check if the battleContainer exists and the panel has not been added
     if (battleContainer && !panelAdded) {
       // Check if the panel already exists
-      if (!battleContainer.querySelector('.panel-content')) {
+      if (!battleContainer.querySelector('.deck-panel')) {
         const panel = document.createElement('div');
         panel.classList.add('deck-panel');
         const headerDiv = createHeader("Recommended Deck", format);
@@ -30,7 +35,7 @@ if (window.location.href === battleHistoryUrl) {
         const recommendedCards = document.createElement('div');
         recommendedCards.classList.add('recommended-cards');
 
-        /* const decks: DeckResponse = await getDecks(99, ["Standard"], [], 'Novice', 'modern', 1, 0);
+        const decks: DeckResponse = await getDecks(99, ["Standard"], [], 'Novice', 'modern', 1, 0);
         const deck = decks.decks[0];
         const cardIds: number[] = [];
         for (const property in deck) {
@@ -70,7 +75,7 @@ if (window.location.href === battleHistoryUrl) {
           recommendedCards.appendChild(cardItem);
         }
 
-        panel.appendChild(recommendedCards); */
+        panel.appendChild(recommendedCards);
         panelWrapper.appendChild(panel);
         battleContainer.appendChild(panelWrapper);
 
