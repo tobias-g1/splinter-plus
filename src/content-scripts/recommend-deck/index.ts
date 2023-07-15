8d863048516d877a630720cea8d39a6773e77ab3
tobias-g1 <tobias.gooney@icloud.com>
2023-07-15 11:51:26 +0100

    Add refresh to recommended deck

--- src/content-scripts/recommend-deck/index.ts
+++ src/content-scripts/recommend-deck/index.ts
@@ -11,6 +11,7 @@ import { createCardItem, createContentHeader, createHeader, extractElementText, 
 const selectTeamUrl = 'https://splinterlands.com/?p=create_team2';
 let panelAdded = false;
 let observer: MutationObserver | null = null;
+let panel: HTMLDivElement | null = null;
 
 // Function to check for the existence of the panel and add it if it doesn't already exist
 const checkPanelExists = async () => {
@@ -23,33 +24,9 @@ const checkPanelExists = async () => {
     const existingPanel = battleContainer.querySelector('.deck-panel');
     if (!existingPanel) {
 
-      const league: string = await getValueFromLocalStorage('league');
       const format: string = await getValueFromLocalStorage('format');
-      const mana: string = extractElementText('.mana-cap')
 
-      // Extract Ruleset
-
-      const createTeamRulesetDiv: any = document.querySelector('#create-team-ruleset > div > div > img');
-      const originalTitle: any = createTeamRulesetDiv.getAttribute('data-original-title');
-      const trimmedText: any = originalTitle.split('-')[0].trim();
-      const ruleset = trimmedText;
-
-      // Extract Elements
-
-      const elementsDiv: any = document.querySelector('#create-team-splinters > .splinter-grid');
-      const splinterElements: any = elementsDiv.querySelectorAll('.splinter');
-      const filteredElements: string[] = [];
-
-      splinterElements.forEach((splinterElement: any) => {
-        const originalTitle = splinterElement.querySelector('img').getAttribute('data-original-title');
-        if (originalTitle.includes('Active')) {
-          // Filter out the ": Active" part and add to the array
-          const filteredElement = originalTitle.split(': Active')[0].trim();
-          filteredElements.push(filteredElement);
-        }
-      });
-
-      const panel = document.createElement('div');
+      panel = document.createElement('div');
       panel.classList.add('deck-panel');
 
       const headerDiv = createHeader("Recommended Deck", format);
@@ -61,59 +38,7 @@ const checkPanelExists = async () => {
       const contentHeader = createContentHeader("Discover the ultimate deck tailored to your ruleset, splinters, mana, league, and game format. Our expert analysis has identified the top-performing decks, ensuring you have access to the very best. Choose from the options below to purchase or rent the cards you need to dominate the game.");
       panelContent.appendChild(contentHeader);
 
-      const recommendedCards = document.createElement('div');
-      recommendedCards.classList.add('recommended-cards');
-
-      const decks: DeckResponse = await getDecks(parseInt(mana), [ruleset], filteredElements, league, format, 1, 0);
-      const deck = decks.decks[0];
-      const cardIds: number[] = [];
-      const cardKeys: string[] = [];
-
-      if ('summoner_id' in deck) {
-        const summonerId = deck['summoner_id'];
-        if (typeof summonerId === 'number') {
-          cardIds.push(summonerId);
-          cardKeys.push('Summoner');
-        }
-      }
-
-      for (const property in deck) {
-        if (property.startsWith('monster') || property === 'summoner_id') {
-          const cardId = deck[property as keyof typeof deck];
-          if (typeof cardId === 'number') {
-            cardIds.push(cardId);
-            cardKeys.push('Monster ' + (parseInt(property.slice(7)) + 1));
-          }
-        }
-      }
-
-      const cardData: CardDetail[] = await getCardDetails(cardIds);
-      const username: string | null = await getUsernameFromLocalStorage();
-
-      let collection: Collection | null = null;
-      if (username) {
-        collection = await getCollection(username);
-      }
-
-      const ownership: CardDetailOwnership[] = cardData.map((card, index) => {
-        const ownedCards: Card[] = [];
-        if (username && collection) {
-          const cards = collection.cards.filter((c) => c.card_detail_id === card.id);
-          ownedCards.push(...cards);
-        }
-
-        return {
-          ...card,
-          cards: ownedCards.sort((a, b) => b.level - a.level),
-          key: cardKeys[index],  // Using the corresponding key from cardKeys array
-        };
-      });
-
-      // Create and append card items to the recommendedCards div
-      for (const card of ownership) {
-        const cardItem = await createCardItem(card, format, card.avg_rating);
-        recommendedCards.appendChild(cardItem);
-      }
+      const recommendedCards = await buildRecommendedCards()
 
       panelContent.appendChild(recommendedCards);
       panel.appendChild(panelContent);
@@ -151,17 +76,106 @@ if (window.location.href === selectTeamUrl) {
   });
 }
 
-// Function to refresh the panel and get the cards again
-export async function refreshDeckPanel() {
-  // Remove the existing panel if it exists
-  const existingPanel = document.querySelector('.deck-panel');
-  if (existingPanel) {
-    existingPanel.remove();
+async function buildRecommendedCards(): Promise<HTMLDivElement> {
+
+  const recommendedCards = document.createElement('div');
+  recommendedCards.classList.add('recommended-cards');
+
+  const league: string = await getValueFromLocalStorage('league');
+  const format: string = await getValueFromLocalStorage('format');
+  const mana: string = extractElementText('.mana-cap')
+
+  // Extract Ruleset
+
+  const createTeamRulesetDiv: any = document.querySelector('#create-team-ruleset > div > div > img');
+  const originalTitle: any = createTeamRulesetDiv.getAttribute('data-original-title');
+  const trimmedText: any = originalTitle.split('-')[0].trim();
+  const ruleset = trimmedText;
+
+  // Extract Elements
+
+  const elementsDiv: any = document.querySelector('#create-team-splinters > .splinter-grid');
+  const splinterElements: any = elementsDiv.querySelectorAll('.splinter');
+  const filteredElements: string[] = [];
+
+  splinterElements.forEach((splinterElement: any) => {
+    const originalTitle = splinterElement.querySelector('img').getAttribute('data-original-title');
+    if (originalTitle.includes('Active')) {
+      // Filter out the ": Active" part and add to the array
+      const filteredElement = originalTitle.split(': Active')[0].trim();
+      filteredElements.push(filteredElement);
+    }
+  });
+
+  const decks: DeckResponse = await getDecks(parseInt(mana), [ruleset], filteredElements, league, format, 1, 0);
+  const deck = decks.decks[0];
+  const cardIds: number[] = [];
+  const cardKeys: string[] = [];
+
+  if ('summoner_id' in deck) {
+    const summonerId = deck['summoner_id'];
+    if (typeof summonerId === 'number') {
+      cardIds.push(summonerId);
+      cardKeys.push('Summoner');
+    }
   }
 
-  // Reset the panelAdded flag
-  panelAdded = false;
+  for (const property in deck) {
+    if (property.startsWith('monster') || property === 'summoner_id') {
+      const cardId = deck[property as keyof typeof deck];
+      if (typeof cardId === 'number') {
+        cardIds.push(cardId);
+        cardKeys.push('Monster ' + (parseInt(property.slice(7)) + 1));
+      }
+    }
+  }
 
-  // Call the checkPanelExists function to add the panel again
-  await checkPanelExists();
+  const cardData: CardDetail[] = await getCardDetails(cardIds);
+  const username: string | null = await getUsernameFromLocalStorage();
+
+  let collection: Collection | null = null;
+  if (username) {
+    collection = await getCollection(username);
+  }
+
+  const ownership: CardDetailOwnership[] = cardData.map((card, index) => {
+    const ownedCards: Card[] = [];
+    if (username && collection) {
+      const cards = collection.cards.filter((c) => c.card_detail_id === card.id);
+      ownedCards.push(...cards);
+    }
+
+    return {
+      ...card,
+      cards: ownedCards.sort((a, b) => b.level - a.level),
+      key: cardKeys[index],  // Using the corresponding key from cardKeys array
+    };
+  });
+
+  // Create and append card items to the recommendedCards div
+  for (const card of ownership) {
+    const cardItem = await createCardItem(card, format, card.avg_rating);
+    recommendedCards.appendChild(cardItem);
+  }
+
+  return recommendedCards;
+
 }
+
+export async function refreshDeckPanel() {
+
+  if (panel) {
+    const cardDiv = panel.querySelector('.recommended-cards');
+    if (cardDiv) {
+      cardDiv.remove();
+    }
+
+    const cards = await buildRecommendedCards();
+
+    const contentDiv = panel.querySelector('.panel-content');
+    if (contentDiv) {
+      contentDiv.appendChild(cards);
+    }
+  }
+}
+
